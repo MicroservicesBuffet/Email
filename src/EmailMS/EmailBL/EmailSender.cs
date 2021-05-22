0f5eb781e6f64a4927b74cf5fb90e0deb8f3c664
ignatandrei <ignatandrei@yahoo.com>
2021-05-22 07:21:38 +0300

    details

--- src/EmailMS/EmailBL/EmailSender.cs
+++ src/EmailMS/EmailBL/EmailSender.cs
@@ -10,16 +10,19 @@ namespace EmailBL
     {
         public void Send()
         {
+            // part of the email template
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse("from_address@example.com"));
             email.To.Add(MailboxAddress.Parse("to_address@example.com"));
             email.Subject = "Test Email Subject";
             email.Body = new TextPart(TextFormat.Html) { Text = "<h1>Example HTML Message Body</h1>" };
 
-            // send email
+            // part of the configuration
             using var smtp = new SmtpClient();
             smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
             smtp.Authenticate("[USERNAME]", "[PASSWORD]");
+
+            // template + configuration
             smtp.Send(email);
             smtp.Disconnect(true);
         }
