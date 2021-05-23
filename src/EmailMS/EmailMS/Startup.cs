7a1685afd9ef37fef1a909d549ba6e6690205bc8
ignatandrei <ignatandrei@yahoo.com>
2021-05-23 20:29:13 +0300

    build and pusblish stopped
    #14

--- src/EmailMS/EmailMS/Startup.cs
+++ src/EmailMS/EmailMS/Startup.cs
@@ -40,9 +40,9 @@ namespace EmailMS
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmailMS v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmailMS v1"));
 
             app.UseHttpsRedirection();
 
