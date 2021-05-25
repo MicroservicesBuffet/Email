05d29f1635163a245e3e92765966a594f6e7e99a
ignatandrei <ignatandrei@yahoo.com>
2021-05-25 21:30:51 +0300

    test

--- src/EmailMS/EmailMS/Controllers/WeatherForecastController.cs
+++ src/EmailMS/EmailMS/Controllers/WeatherForecastController.cs
@@ -24,16 +24,9 @@ namespace EmailMS.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "test";
         }
     }
 }
