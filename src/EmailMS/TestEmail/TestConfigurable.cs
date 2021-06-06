ea463fe6c73c7aa91cdb89c9370ac330a340b403
ignatandrei <ignatandrei@yahoo.com>
2021-06-06 13:06:44 +0300

    now correct test

--- src/EmailMS/TestEmail/TestConfigurable.cs
+++ src/EmailMS/TestEmail/TestConfigurable.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -22,11 +23,11 @@ namespace TestEmail
         {
 
             await Runner.AddSteps(
-                Given_Create_FileSystem_WithPlugins,
-                When_Create_Configurable_EmailSettings,
-                Then_Can_Found_SMTPProviders
+                _=> Given_Create_FileSystem_WithPlugins(),
+                _ => When_Create_Configurable_EmailSettings(),
+                _ => Then_Can_Found_SMTPProviders(),
+                _ => And_The_Number_of_SMTPProviders_is(2)
             )
-            .AddStep(nameof(And_The_Number_of_SMTPProviders_is), _ => And_The_Number_of_SMTPProviders_is(2))
             .RunAsync();
 
 
