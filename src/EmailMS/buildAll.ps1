9a54e6d1176b0aefdf4a21a9b10c101261cf42e9
ignatandrei <ignatandrei@yahoo.com>
2021-06-15 08:33:32 +0300

    clean and run

--- src/EmailMS/buildAll.ps1
+++ src/EmailMS/buildAll.ps1
@@ -1,6 +1,7 @@
+dotnet clean
 dotnet restore
-dotnet build
+dotnet build -c Debug --no-restore
 Get-ChildItem
 copy-item -Path ".\SimpleSMTP\bin\Debug\net5.0\" -Destination ".\SenderEmail\wwwroot\plugins\smtpProviders\SimpleSMTP\" -Recurse -Force
 copy-item -Path ".\EmailSmtpClientGmail\bin\Debug\net5.0\" -Destination ".\SenderEmail\wwwroot\plugins\smtpProviders\EmailSmtpClientGmail\" -Recurse -Force
-
+dotnet run --project --project SenderEmail --no-build
