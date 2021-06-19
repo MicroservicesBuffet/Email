235004444886716065c615c35d0131a1e7d1a9ec
ignatandrei <ignatandrei@yahoo.com>
2021-06-19 16:39:00 +0300

    middleware configuration

--- src/EmailMS/SenderEmail/General/MiddlewareConfigure.cs
+++ src/EmailMS/SenderEmail/General/MiddlewareConfigure.cs
@@ -1,7 +1,9 @@
 ﻿using ConfigureMS;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,27 +12,57 @@ namespace SenderEmail.General
     public class MiddlewareConfigure : IMiddleware
     {
         private readonly IStartConfigurationMS config;
+        private readonly IRepoMS data;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public MiddlewareConfigure(IStartConfigurationMS config)
+        public MiddlewareConfigure(IStartConfigurationMS config, IRepoMS data, IWebHostEnvironment webHostEnvironment)
         {
             this.config = config;
+            this.data = data;
+            this.webHostEnvironment = webHostEnvironment;
         }
-        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
+            if (!config.IsConfigured())
+            try
+            {
+                var pluginsFolder = Path.Combine(webHostEnvironment.WebRootPath, "plugins");
+                await foreach (var item in config.StartFinding(pluginsFolder))
+                {
+                    //ModelState.AddModelError(item.MemberNames.FirstOrDefault() ?? "error", item.ErrorMessage);
+                }
+                await config.LoadData(data);
+            }
+            catch
+            {
+                //do nothing
+            }
             if (config.IsConfigured())
-                return next(context);
-            
-            if (context.Request.Path.Value?.Contains("StartConfigure")??false)
-                return next(context);
+            {
+                await next(context);
+                return;
+            }
+
+            if (context.Request.Path.Value?.Contains("StartConfigure") ?? false)
+            {
+                await next(context);
+                return;
+            }
             if (context.Request.Path.Value?.EndsWith("css") ?? false)
-                return next(context);
+            {
+                await next(context);
+                return;
+            }
             if (context.Request.Path.Value?.EndsWith("js") ?? false)
-                return next(context);
+            {
+                await next(context);
+                return;
+            }
 
             //if it is not configured , go to Start Configure
             context.Response.Redirect("/StartConfigure/Index");
             context.Response.StatusCode = StatusCodes.Status302Found;
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
             //return next(context);
         }
     }
