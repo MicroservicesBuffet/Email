b5149c21286c99cabd94c69940abefe1b4c18c51
ignatandrei <ignatandrei@yahoo.com>
2021-06-20 17:02:19 +0300

    swagger

---
+++ src/EmailMS/SenderEmail/Controllers/SendSimpleEmail.cs
@@ -0,0 +1,31 @@
+﻿using ConfigureMS;
+using EmailConfigurator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenderEmail.Controllers
+{
+    public class SimpleSendEmail
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public string subject { get; set; }
+        public string body { get; set; }
+    }
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class SendSimpleEmail : ControllerBase
+    {
+        [HttpPost]
+        public Task SendEmail([FromServices]IStartConfigurationMS config, [FromBody]SimpleSendEmail send)
+        {
+            var client = config.ChoosenProviderData as IEmailSmtpClient;
+            return client.Client().SendMailAsync(send.from, send.to, send.subject, send.body);
+        }
+    }
+}
--- src/EmailMS/SenderEmail/Startup.cs
+++ src/EmailMS/SenderEmail/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SenderEmail.General;
 using System;
 using System.Collections.Generic;
@@ -32,7 +33,11 @@ namespace SenderEmail
             services.AddSingleton<IRepoMS>(s => new RepoMSFile("email.json", s.GetRequiredService<IFileSystem>()));
             services.AddSingleton<IStartConfigurationMS, ConfigureEmail>();
             services.AddControllersWithViews();
-            
+            services.AddApiVersioning();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmailSender", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +53,11 @@ namespace SenderEmail
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseMiddleware<MiddlewareConfigure>();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkinnyControllerTest v1"));
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
